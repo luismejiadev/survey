2e27d995f041c5ba3016d06c195a49cff816606c
Luis Mejia <lcmejia19@gmail.com>
2019-07-28 04:45:14 -0600

    add admin settings

---
+++ settings/admin/admin.py
@@ -0,0 +1,37 @@
+from settings.base import *
+
+
+ADMIN_TOOLS_APPS = [
+    'admin_tools',
+    'admin_tools.dashboard'
+]
+
+INSTALLED_APPS = ADMIN_TOOLS_APPS + INSTALLED_APPS
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'OPTIONS': {
+            'loaders': [
+                    'admin_tools.template_loaders.Loader',
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+            ],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+ALLOWED_HOSTS = []
+DEBUG = True
