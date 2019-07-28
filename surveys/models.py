700068730221f2cc7ad1aeb2c31aa6e78631d2d9
Luis Mejia <lcmejia19@gmail.com>
2019-07-28 04:41:46 -0600

    add slug field to Question model

--- surveys/models.py
+++ surveys/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=20)
     pub_date = models.DateTimeField('date published')
     is_active = models.BooleanField(default=True)
 
@@ -13,7 +14,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0, editable=False)
     is_active = models.BooleanField(default=True)
-
 
 class UserChoice(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
