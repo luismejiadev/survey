221e4c561bb127327a3624c4be7e3f0092afa7a4
Luis Mejia <lcmejia19@gmail.com>
2019-07-29 16:58:30 -0600

    add activate/deactivate actions

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Question, Choice
+from django.contrib import messages as flash_messages
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -10,6 +11,22 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['question_text', 'pub_date', 'is_active']
     list_filter = ['is_active']
     inlines = [ChoiceInline]
+    actions = ['active_questions', 'deactive_questions']
+
+    def active_questions(self, request, queryset):
+        count = queryset.filter(is_active=False).update(is_active=True)
+        message = "{0} questions activated".format(count)
+        # Read Message Framework
+        # https://docs.djangoproject.com/en/2.2/ref/contrib/messages/#adding-a-message
+        flash_messages.success(request, message)
+
+    def deactive_questions(self, request, queryset):
+        count = queryset.filter(is_active=True).update(is_active=False)
+        message = "{0} questions deactivated".format(count)
+        # Read Message Framework
+        # https://docs.djangoproject.com/en/2.2/ref/contrib/messages/#adding-a-message
+        flash_messages.error(request, message)
+
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ['choice_text', 'question', 'votes', 'is_active']
