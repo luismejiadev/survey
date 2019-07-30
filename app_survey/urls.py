1da3ee495012c7240b3e73064c3d05b38e1e3004
Luis Mejia <lcmejia19@gmail.com>
2019-07-30 16:35:00 -0600

    render static and media for dev

--- app_survey/urls.py
+++ app_survey/urls.py
@@ -1,23 +1,20 @@
-"""app_survey URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin_tools/', include('admin_tools.urls')),
     path('admin/', admin.site.urls),
     path('', include('surveys.urls')),
 ]
+
+
+if settings.DEBUG:
+    # Read: Serving static files during development
+    # https://docs.djangoproject.com/en/2.2/howto/static-files/#serving-static-files-during-development
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Read: Serving files uploaded by a user during development
+    # https://docs.djangoproject.com/en/2.2/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
