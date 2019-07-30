1ab908d13e19bc71163f54ca59e8d637acdfd5ba
Luis Mejia <lcmejia19@gmail.com>
2019-07-30 16:31:20 -0600

    fix: timedelta

--- surveys/utils.py
+++ surveys/utils.py
@@ -1,5 +1,5 @@
 import datetime
 
 SURVEY_DAYS = 15
-survey_end_date = lambda: datetime.date.today() + datetime.interval(days=SURVEY_DAYS)
+survey_end_date = lambda: datetime.date.today() + datetime.timedelta(days=SURVEY_DAYS)
 
